2a4a377de201a4a5680651c4e105b28f9bb14c79
rhish <rhish9h@gmail.com>
2019-07-04 00:22:18 +0530

    update data php script

---
+++ updateData.php
@@ -0,0 +1,36 @@
+<?php
+    //push data into the orders tables
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Credentials: true ");
+    header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+    header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') { //post not working, so used get
+        include 'config.php';
+
+        $sno = $_GET['sno'] ?? '';
+        $delField = $_GET['delField'] ?? 'serial number';
+        $tableName = $_GET['tableName'] ?? '';
+        $fieldValues = json_decode($_GET['fieldValues']) ?? array('');  //decode the array sent - contains fields and values
+        $query_fieldVal = '';
+
+        // create a set of fields for query
+
+        foreach ($fieldValues as $field) {
+            $query_fieldVal .= '`'.$field[0]."` = '".$field[1]."', ";  // add backticks and comma+space
+        }
+        $query_fieldVal = rtrim($query_fieldVal, ', '); //trim off the last comma+space
+
+        // put table name, fields and values in query
+        // UPDATE `order details` SET `suborder id` = 'test1', `size` = 'test2' WHERE `order details`.`serial number` = 18;
+        $query = "UPDATE `$tableName` SET $query_fieldVal WHERE `$tableName`.`$delField` = $sno;";
+
+        $userData = mysqli_query($conn, $query);
+        
+        if(!$userData) {
+            echo $query;
+            var_dump($_REQUEST);
+            die (mysqli_error($conn));
+        }
+    }
+    
